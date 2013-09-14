36c7a35b77cd859878cf4944542b8b4340eec3dc
aulia <kingmalprax@gmail.com>
2013-09-15 05:58:33 +0800

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MySandwiches::Application.config.secret_key_base = 'b00dec83b79ca6225e970dd98e185ced92e88a4abbfe082e81d92ee508023920dba362df9ddcc6b686d8ffe8382a97e33ce0a2413f68eddba0b0b5d132202562'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MySandwiches::Application.config.session_store :cookie_store, key: '_my_sandwiches_session'
