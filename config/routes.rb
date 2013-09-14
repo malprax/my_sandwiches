f59f9bf08bfdd9d3c33931a15c677a2ad683d040
aulia <kingmalprax@gmail.com>
2013-09-15 06:28:01 +0800

    comfig root_routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ MySandwiches::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'order_pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
