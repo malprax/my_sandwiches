2974ad33cb9267fd25542f914f4ee1ffa3c22d55
aulia <kingmalprax@gmail.com>
2013-10-01 14:06:20 +0800

    try to fix error db:push

--- db/migrate/20130921222726_add_spreads_to_order_pages.rb
+++
@@ -1,7 +0,0 @@
-class AddSpreadsToOrderPages < ActiveRecord::Migration
-  def change
-    add_column :order_pages, :spreads, :string
-    add_column :order_pages, :vegetables, :string
-    add_column :order_pages, :sauces, :string
-  end
-end
--- db/migrate/20130930050624_create_sauces.rb
+++ db/migrate/20130930050624_create_sauces.rb
@@ -2,7 +2,7 @@ class CreateSauces < ActiveRecord::Migration
   def change
     create_table :sauces do |t|
       t.string :name
-      t.string :price
+      t.integer :price
 
       t.timestamps
     end
