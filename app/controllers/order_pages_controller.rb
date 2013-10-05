4485d6879e53a3eef38a82aef88f90e2e70aa006
aulia <kingmalprax@gmail.com>
2013-10-06 06:09:14 +0800

    fix finishing

--- app/controllers/order_pages_controller.rb
+++ app/controllers/order_pages_controller.rb
@@ -38,9 +38,11 @@ class OrderPagesController < ApplicationController
       if @order_page.save
         format.html { redirect_to @order_page, notice: 'Order page was successfully created.' }
         format.json { render action: 'show', status: :created, location: @order_page }
+        format.js
       else
         format.html { render action: 'new' }
         format.json { render json: @order_page.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end
@@ -52,9 +54,11 @@ class OrderPagesController < ApplicationController
       if @order_page.update(order_page_params)
         format.html { redirect_to order_page_path, notice: 'Order page was successfully updated.' }
         format.json { head :no_content }
+        format.js
       else
         format.html { render action: 'edit' }
         format.json { render json: @order_page.errors, status: :unprocessable_entity }
+        format.js
       end
     end
   end
@@ -66,6 +70,7 @@ class OrderPagesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to order_pages_url }
       format.json { head :no_content }
+      format.js
     end
   end
 
