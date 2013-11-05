c07425da069b19d6b32793682b3a8377c9c64567
aulia <kingmalprax@gmail.com>
2013-11-06 07:40:03 +0800

    add load and authorize in order_page controller

--- app/controllers/order_pages_controller.rb
+++ app/controllers/order_pages_controller.rb
@@ -1,5 +1,5 @@
 class OrderPagesController < ApplicationController
-  # load_and_authorize_resource
+  load_and_authorize_resource
   before_action :authenticate_user!
   before_action :set_order_page, only: [:show, :edit, :update, :destroy]
 
